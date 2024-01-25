d8d2752a567d804545bcc46e441c64564baed4df
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-01-25 16:03:27 -0500

    First pass of playRound (does not replay on tie)

--- script.js
+++ script.js
@@ -16,3 +16,27 @@ function getComputerChoice()
     else
         return 'Scissors';
 }
+
+
+// Play one round of Rock-Paper-Scissors (re-do round on tie)
+function playRound(playerChoice, computerChoice)
+{
+    // Convert player choice to title case
+    const playerConverted = (
+        playerChoice.charAt(0).toUpperCase() +
+        playerChoice.substring(1).toLowerCase()
+    );
+
+    // Retrieve winner based on array indexing
+    const moves = ['Rock', 'Paper', 'Scissors'];
+    const playerIndex = moves.indexOf(playerConverted);
+    const computerIndex = moves.indexOf(computerChoice);
+
+    // Check for winner
+    if (playerIndex === (computerIndex + 1) % 3)
+        return `You Win! ${playerConverted} beats ${computerChoice}.`
+    else if (computerIndex === (playerIndex + 1) % 3)
+        return `You Lose! ${computerChoice} beats ${playerConverted}.`
+    else
+        return `It's a Tie! ${playerConverted} is the same as ${computerChoice}.`
+}
