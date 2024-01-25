531d6739861437b4ece0b0670851ddce1bdfdf6f
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-01-25 15:31:39 -0500

    Implement getComputerChoice

--- script.js
+++ script.js
@@ -3,3 +3,16 @@
     The Odin Project: Foundations
     Rock Paper Scissors
 */
+
+
+// Retrieve randomized choice for computer player
+function getComputerChoice()
+{
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0)
+        return 'Rock';
+    else if (choice === 1)
+        return 'Paper';
+    else
+        return 'Scissors';
+}
